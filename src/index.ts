6ca59474b115fe54670b423254663a6d2734f72c
Holden Whitehead <holdenw.dev@gmail.com>
2025-01-09 16:09:45 -0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import fp from 'fastify-plugin'
+import openTelemetryPlugin, {
+  OpenTelemetryPluginOptions
+} from '@autotelic/fastify-opentelemetry'
+
+export default fp<OpenTelemetryPluginOptions>(
+  async (fastify, _opts) => {
+    fastify.register(openTelemetryPlugin, { wrapRoutes: true })
+  }
+)
